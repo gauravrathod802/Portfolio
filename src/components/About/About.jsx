65d28e3a4ddad276c5ba671d94b51abf9ff27ee6
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2023-12-28 14:17:01 +0530

    some responsiveness done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                     <img src={ABOUT} alt="" />
                 </div>
                 <div className="about-me-text">
-                    <h1>Developer</h1>
+                    <h2>Developer</h2>
                     <p>
                         Hi there I'am Gaurav Rathod, i have completed my bachelors from Sknsits, Lonavala pune. I am a front-end web developer I can provide clean code and pixel perfect design. I also make the website more & more interactive. I can provide clean code and pixel perfect design. I also make the website more & more interactive with web animations.A responsive design makes your website accessible to all users, regardless of their device.</p>
                 </div>
